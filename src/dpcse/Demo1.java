25a34dda30d5a1cdb5b216dec6fde60d1e37f44e
Hemamalinipilli <84316344+Hemamalinipilli@users.noreply.github.com>
2022-10-17 15:05:10 +0530

    one more sop

--- src/dpcse/Demo1.java
+++ src/dpcse/Demo1.java
@@ -5,6 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("DURGA PRASAD");
+		System.out.print("DURGA PRASAD");
+
 
 	}
 
